59148f4ec2b08808fd0b28c6af401acbc1567efe
Houston4444 <picotmathieu@gmail.com>
2026-05-17 16:38:55 +0200

    adapt resources used in donate dialog

--- src/donate_dialog.py
+++ src/donate_dialog.py
@@ -4,6 +4,9 @@ from qtpy.QtWidgets import QDialog
 
 from patchbay.tools_widgets import is_dark_theme
 
+import resourcer
+from resources import scalables
+
 from ui.donations import Ui_Dialog
 
 
@@ -12,13 +15,12 @@ class DonationsDialog(QDialog):
         super().__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        
-        self.ui.checkBox.setVisible(False)
-        
-        dark = '-dark' if is_dark_theme(self) else ''
+        self.ui.toolButtonDonate.setIcon(
+            resourcer.icon(scalables.misc.LIBERAPAY_LOGO_BLACK_ON_YELLOW))
         self.ui.toolButtonImage.setIcon(
-            QIcon(f':scalable/breeze{dark}/handshake-deal.svg'))
-        
+            resourcer.icon(
+                scalables.misc.HANDSHAKE_DEAL, dark=is_dark_theme(self)))
+        self.ui.checkBox.setVisible(False)        
         self.ui.toolButtonDonate.clicked.connect(self._donate)
         
     def _donate(self):
